a7f3029d9bba6e510c118cc382095980fbc94dae
Alex Lezard <ajl205@cam.ac.uk>
2018-11-15 10:15:27 +0000

    tests

---
+++ first_test_.ino
@@ -0,0 +1,11 @@
+sensorPin =A0
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+}
